a20d094631055f7b08efb1da86719785a472f56f
Jeanpier Mendoza <jeanpiermendoza@outlook.com>
2021-05-30 17:10:21 -0500

    HeroScreen hecho

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,10 +1,55 @@
 import React from "react";
+import { Redirect, useParams } from "react-router";
+import { getHeroById } from "../../selectors/getHeroById";
 
-export const HeroScreen = () => {
+export const HeroScreen = ({ history }) => {
+  const { heroId } = useParams();
+  const hero = getHeroById(heroId);
+
+  // ! Valida que haya encontrado un héroe
+  if (!hero) return <Redirect to="/" />;
+
+  const { superhero, publisher, alter_ego, first_appearance, characters } =
+    hero;
+
+  const handleReturn = () => {
+    if (history.length <= 2) {
+      history.push("/");
+      return;
+    }
+    history.goBack();
+  };
+
   return (
-    <div>
-      <h1>Hero Screen</h1>
-      <hr />
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={`../assets/heroes/${heroId}.jpg`}
+          className="img-thumbnail"
+          alt={superhero}
+        />
+      </div>
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush mb-3">
+          <li className="list-group-item">
+            <b>Alter ego:</b> {alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher:</b> {publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First appeareance:</b> {first_appearance}
+          </li>
+        </ul>
+
+        <h5>Characters</h5>
+        <p>{characters}</p>
+
+        <button className="btn btn-outline-info" onClick={handleReturn}>
+          Return
+        </button>
+      </div>
     </div>
   );
 };
