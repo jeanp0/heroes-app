a2fc7ea68f578ba2ab882f972e10afe1255376ec
Jeanpier Mendoza <jeanpiermendoza@outlook.com>
2021-06-04 21:33:57 -0500

    .

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,16 +1,12 @@
 import React, { useMemo } from "react";
 import { Redirect, useParams } from "react-router";
 import { heroImages } from "../../helpers/heroImages";
-import test from "../../helpers/test";
 import { getHeroById } from "../../selectors/getHeroById";
-// import batman from "../../assets/heroes/dc-batman.jpg"; // estático
 
 export const HeroScreen = ({ history }) => {
   const { heroId } = useParams();
   const hero = useMemo(() => getHeroById(heroId), [heroId]);
 
-  test();
-  // ! Valida que haya encontrado un héroe
   if (!hero) return <Redirect to="/" />;
 
   const { superhero, publisher, alter_ego, first_appearance, characters } =
@@ -28,8 +24,6 @@ export const HeroScreen = ({ history }) => {
     <div className="row mt-5">
       <div className="col-4">
         <img
-          // src={`../assets/heroes/${heroId}.jpg`} // desde public/assets
-          // src={batman} // static import
           src={heroImages(`./${heroId}.jpg`).default}
           className="img-thumbnail animate__animated animate__fadeInLeft"
           alt={superhero}
